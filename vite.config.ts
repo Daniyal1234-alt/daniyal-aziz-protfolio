a99fa637000fec682e19d8e05480d2264ced0d5d
i220753-sudo <i220753@nu.edu.pk>
2025-09-12 16:01:44 +0500

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // 👇 IMPORTANT for GitHub Pages
+  base: "/daniyal-aziz-protfolio/",
 }));
